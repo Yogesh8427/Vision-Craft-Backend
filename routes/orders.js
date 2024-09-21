3426bafa50f0284e0722fe1809ae70a0cf9578c9
YOGESH KUMAR <yk24150@gmail.com>
2024-09-21 19:38:57 +0530

    change in order.js file

--- routes/orders.js
+++ routes/orders.js
@@ -6,13 +6,13 @@ const db = dbconnection();
 //1- get your orders details
 router.get('/getorders', fetchuser, (req, res) => {
     try {
-        const sql = `select * from orders where userid=${req.user}`
-        db.query(sql,(err,result)=>{
-            if(err){
+        const sql = `select * from Orders where userid=${req.user}`
+        db.query(sql, (err, result) => {
+            if (err) {
                 console.log(err);
                 return res.status(500).json({ err });
             }
-            return res.json({status:200,data:result});
+            return res.json({ status: 200, data: result });
         })
     } catch (error) {
         console.log("error", error);
@@ -20,16 +20,17 @@ router.get('/getorders', fetchuser, (req, res) => {
     }
 })
 //make order cancle throught 
-router.post("/cancleorder",fetchuser,(req,res)=>{
+router.post("/cancleorder", fetchuser, (req, res) => {
     try {
-        const {item_id,order_id,cancleReason}=req.body;
-        const sql=`update orders set order_status="Cancle",cancleReason="${cancleReason}" where userid=${req.user} and item_id=${item_id} and order_id="${order_id}";`
-        db.query(sql,(err,result)=>{
-            if(err){
+        const { order_id, cancleReason,admin_id} = req.body;
+        const sql1 = `update orders set order_status="Cancelled",cancleReason="${cancleReason}" where userid=${req.user} and order_id="${order_id}";`
+        const sql2 = `update orders set order_status="Cancelled",cancleReason="${cancleReason}" where admin_id=${req.user} and order_id="${order_id}";`
+        db.query(admin_id?sql2:sql1, (err, result) => {
+            if (err) {
                 console.log(err);
                 return res.status(500).json({ err });
-            }else{
-                return res.json({status:200,msg:"Your Canceld the Order"})
+            } else {
+                return res.json({ status: 200, msg: "Your Canceld the Order" })
             }
         })
     } catch (error) {
@@ -41,12 +42,61 @@ router.post("/cancleorder",fetchuser,(req,res)=>{
 router.get('/getAdminorders', fetchuser, (req, res) => {
     try {
         const sql = `select * from orders where admin_id=${req.user}`
-        db.query(sql,(err,result)=>{
-            if(err){
+        db.query(sql, (err, result) => {
+            if (err) {
                 console.log(err);
                 return res.status(500).json({ err });
             }
-            return res.json({status:200,data:result});
+            return res.json({ status: 200, data: result });
+        })
+    } catch (error) {
+        console.log("error", error);
+        res.json({ status: 400 });
+    }
+})
+
+router.post("/getbuyproducts", fetchuser, (req, res) => {
+    try {
+        const { order_id } = req.body;
+        const sql1 = `select * from order_details where order_id= "${order_id}"`
+        db.query(sql1, (error, result) => {
+            if (error) {
+                console.log(error);
+                return res.status(500).json({ error });
+            } else {
+                const arr = [];
+                result.map((item, index) =>
+                    db.query(`select * from products where item_id=${item.item_id}`, (err, result1) => {
+                        if (err) {
+                            console.log(error);
+                            return res.status(500).json({ err });
+                        } else {
+                            result1[0].Quantity = item.Quantity;
+                            arr.push(result1[0]);
+                            if (result.length == index + 1)
+                                return res.json(arr);
+                        }
+                    })
+                )
+            }
+        })
+    } catch (error) {
+        console.log("error", error);
+        res.json({ status: 400 });
+    }
+
+})
+router.post("/setorderstatus", (req, res) => {
+    try {
+        const { order_id, status } = req.body;
+        const sql=`update Orders set order_status="${status}" where order_id="${order_id}"`;
+        db.query(sql,(error,result)=>{
+            if(error){
+                console.log(error);
+                return res.status(500).json({ error });
+            }else{
+                return res.json({status:200,msg:"Updated Succesfully"})
+            }
         })
     } catch (error) {
         console.log("error", error);
